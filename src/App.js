6133912f2517a73323cb9d4c391d0e58fc08dd29
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-16 21:32:00 +0100

    create button scroll to top on the page.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import {RestaurantList} from "./Components/Restaurant/RestaurantList";
 import {InDataMemory} from "./InDataMemory";
 
 /** IMPORT REACT SCROLL **/
-import { Link, Element} from 'react-scroll';
+import React, {useState, useEffect} from 'react';
+import { Link, Element, animateScroll as scroll}   from 'react-scroll';
 
 const store = new RestaurantStore(new InDataMemory());
 
 function App() {
+    const [hiddenScrollButton, setValueHidden] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', (event) => {
+            const scrollY = window.scrollY;
+            setValueHidden(!!scrollY)
+        })
+    }, []);
+
+    function scrollToTop() {
+        scroll.scrollToTop();
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -23,10 +37,15 @@ function App() {
                         <div className="row">
                             <div className="col-lg-5 col-md-12 mb-5">
                                 <h1 className="animate animate-fade fade-in-top">Trouvez le meilleur restaurant près de chez vous</h1>
-                                <p className="animate animate-fade fade-in-left delay-1">Vous chercher le restaurants le mieux noté autour de chez vous pour organiser votre sortie. Nous recherchons les meilleurs restaurants a votre place. A l’aide de votre géolocalisation et avec votre accord tous ce fait automatiquement.</p>
+                                <p className="animate animate-fade fade-in-left delay-1">
+                                    Vous chercher le restaurants le mieux noté autour de chez vous pour organiser votre sortie. Nous recherchons les meilleurs restaurants a votre place.
+                                    A l’aide de votre géolocalisation et avec votre accord tous ce fait automatiquement.
+                                </p>
                                 <div className="animate animate-fade fade-in-bottom delay-2">
-                                    <Link activeClass="link-active" className="btn btn-primary mr-3" to="anchor-list-restaurant" spy={true} duration={500} smooth={true}><span className="icon icon_food-multiple" />Restaurant le plus proche</Link>
-                                    <a href="#" className="btn btn-primary"><span className="icon icon_food-multiple"></span>Tous les restaurants</a>
+                                    <Link activeClass="link-active" className="btn btn-primary mr-3" to="anchor-list-restaurant" spy={true} duration={500} smooth={true}>
+                                        <span className="icon icon_food-multiple" />Restaurant le plus proche
+                                    </Link>
+                                    <a href="test.html" className="btn btn-primary"><span className="icon icon_food-multiple"></span>Tous les restaurants</a>
                                 </div>
                             </div>
 
@@ -53,6 +72,8 @@ function App() {
                     </div>
                 </div>
             </div>
+
+            { hiddenScrollButton && (<button className="btn btn-primary btn-toggle-top shadow-sm" onClick={scrollToTop}><i className="bi bi-arrow-up"/></button>) }
         </div>
   );
 }
