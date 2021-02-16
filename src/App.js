7c2c012b21d88836518195ec6e004dd3c6e64020
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-16 16:01:58 +0100

    Add button on the main page.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
 import './back.svg';
 import './present.svg';
-import './App.css';
 
 import {RestaurantStore} from "./Stores/RestaurantStore";
 import {RestaurantList} from "./Components/Restaurant/RestaurantList";
 import {InDataMemory} from "./InDataMemory";
 
 /** IMPORT REACT SCROLL **/
-import React, {useEffect} from "react";
-import * as Scroll from 'react-scroll';
-import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Link, Element} from 'react-scroll';
 
 const store = new RestaurantStore(new InDataMemory());
 
 function App() {
-    useEffect(() => {
-        Events.scrollEvent.register('begin', () => {
-            console.log('begin', arguments);
-        })
-
-        Events.scrollEvent.register('end', () => {
-            console.log('end', arguments);
-        })
-
-        scrollSpy.update();
-
-        return () => {
-            Events.scrollEvent.remove('begin');
-            Events.scrollEvent.remove('end');
-        }
-    }, [])
-
     return (
         <div className="App">
             <header className="App-header">
-                <div className="site-information">
+                <div className="header pt-3 pb-3">
                     <div className="container">
-                        <h1>Trouvez le meilleur restaurant près de chez vous</h1>
-                        <p>Vous chercher le restaurants le mieux noté autour de chez vous pour organiser votre sortie. Nous recherchons les meilleurs restaurants a votre place. A l’aide de votre géolocalisation et avec votre accord tous ce fait automatiquement.</p>
-                        <Link activeClass="link-active" className="btn btn-primary mr-3" to="anchor-list-restaurant" spy={true} duration={500} smooth={true}><span className="icon icon_food-multiple" />Restaurant le plus proche</Link>
-                        <a href="#" className="btn btn-primary"><span className="icon icon_food-multiple"></span>Tous les restaurants</a>
+                        <input type="text" className="search-field" placeholder="search"/>
                     </div>
                 </div>
-                <div className="site-visuel">
-                    <img src="present.svg" alt="presentation"/>
+                <div className="site-information">
+                    <div className="header-container container d-flex justify-content-center align-items-center">
+                        <div className="row">
+                            <div className="col-lg-5 col-md-12 mb-5">
+                                <h1 className="animate animate-fade fade-in-top">Trouvez le meilleur restaurant près de chez vous</h1>
+                                <p className="animate animate-fade fade-in-left delay-1">Vous chercher le restaurants le mieux noté autour de chez vous pour organiser votre sortie. Nous recherchons les meilleurs restaurants a votre place. A l’aide de votre géolocalisation et avec votre accord tous ce fait automatiquement.</p>
+                                <div className="animate animate-fade fade-in-bottom delay-2">
+                                    <Link activeClass="link-active" className="btn btn-primary mr-3" to="anchor-list-restaurant" spy={true} duration={500} smooth={true}><span className="icon icon_food-multiple" />Restaurant le plus proche</Link>
+                                    <a href="#" className="btn btn-primary"><span className="icon icon_food-multiple"></span>Tous les restaurants</a>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-7 col-md-12">
+                                <div className="site-visuel animate animate-fade fade-in-right delay-3">
+                                    <img src="present.svg" alt="presentation"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </header>
 
@@ -51,7 +45,7 @@ function App() {
                 <div className="row">
                     <div className="col-md-6">
                         <Element name="anchor-list-restaurant">
-                            <h2>Liste des reseataurants</h2>
+                            <h2>Liste des restaurants</h2>
                             <RestaurantList store={store}/>
                         </Element>
                     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css';
+import './App.css';
 
 
 import React from 'react';
