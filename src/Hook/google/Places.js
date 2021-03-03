cb3f0c7ea1b6e7775a2c807bce9e2cda60703490
Joudrier Kevin <kev.joudrier@gmail.com>
2021-03-03 18:44:54 +0100

    Clean and comment method get interest for the google map.

--- src/Hook/google/Places.js
+++ src/Hook/google/Places.js
@@ -1,6 +1,12 @@
-import {useEffect} from 'react';
-import {useLoadedService} from "./API";
-
+/**
+ * Récupére la liste des points d'intêret avec google place
+ *
+ * @param coordinates : Cordonnées du centre pour créer le rayons de recherche
+ * @param type : Type de point d'intêret que l'on recherche
+ * @param radius : Rayon d'action
+ * @param map : Instance de la map google
+ * @param callback : Callable qui renvoie les données
+ **/
 export function getInterestForCoordinates(coordinates, type, radius = 50, map, callback) {
     const request = {
         location: new window.google.maps.LatLng(coordinates.lat, coordinates.lng),
