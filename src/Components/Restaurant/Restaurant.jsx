a66aeac3efcd09df269ce7b7e051cc04c79491e6
Joudrier Kevin <kev.joudrier@gmail.com>
2021-03-04 16:12:36 +0100

    Affichage d'un restaurant.

--- src/Components/Restaurant/Restaurant.jsx
+++ src/Components/Restaurant/Restaurant.jsx
@@ -1,44 +1,50 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useEffect} from 'react';
 import {Link, useParams} from 'react-router-dom'
-import {Stars} from "./Rating";
+import {getDetailsInterest} from "../../Hook/google/Places";
 
 export function Restaurant(props) {
-    let {id} = useParams();
-    let store = props.store;
+    // let {id} = useParams();
+    // let store = props.store;
+    //
+    // const restaurantMemo = useMemo(function() {
+    //     return store.getRestaurant(id);
+    // }, [store, id])
+    //
+    // function numberOpinion(opinions) {
+    //     return opinions.length;
+    // }
+    //
+    // function displayOpinions(opinions) {
+    //     return opinions.map((opinion) => {
+    //         return <div className="opinion">
+    //             <Stars stars={opinion.stars} />
+    //             <p className="opinion__rating">{opinion.stars}</p>
+    //             <p>{opinion.comment}</p>
+    //             <hr/>
+    //         </div>;
+    //     });
+    // }
 
-    const restaurantMemo = useMemo(function() {
-        return store.getRestaurant(id);
-    }, [store, id])
-
-    function numberOpinion(opinions) {
-        return opinions.length;
-    }
-
-    function displayOpinions(opinions) {
-        return opinions.map((opinion) => {
-            return <div className="opinion">
-                <Stars stars={opinion.stars} />
-                <p className="opinion__rating">{opinion.stars}</p>
-                <p>{opinion.comment}</p>
-                <hr/>
-            </div>;
-        });
-    }
+    // return <div className="container mt-3">
+    //     <Link to="/">Home</Link>
+    //
+    //     <h1>{restaurantMemo.restaurantName}</h1>
+    //
+    //     <p className="react-restaurant__address card-text text-left"><i className="bi bi-geo-alt-fill" />{restaurantMemo.address}</p>
+    //
+    //     <div className="col-md-12 card p-3">
+    //         <div className="container">
+    //             <h3>Avis :</h3> Il y a {numberOpinion(restaurantMemo.ratings)} avis.
+    //             <hr/>
+    //
+    //             { displayOpinions(restaurantMemo.ratings) }
+    //         </div>
+    //     </div>
+    // </div>;
 
     return <div className="container mt-3">
         <Link to="/">Home</Link>
 
-        <h1>{restaurantMemo.restaurantName}</h1>
-
-        <p className="react-restaurant__address card-text text-left"><i className="bi bi-geo-alt-fill" />{restaurantMemo.address}</p>
-
-        <div className="col-md-12 card p-3">
-            <div className="container">
-                <h3>Avis :</h3> Il y a {numberOpinion(restaurantMemo.ratings)} avis.
-                <hr/>
-
-                { displayOpinions(restaurantMemo.ratings) }
-            </div>
-        </div>
+        {/*{console.log(restaurantMemo)}*/}
     </div>;
 }
